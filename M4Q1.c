31b2eadc1160f3ff3a3c8a882ffb62b58014196e
Sanketmajithiya <143784345+Sanketmajithiya@users.noreply.github.com>
2024-01-20 11:56:04 +0530

    Add files via upload

---
+++ M4Q1.c
@@ -0,0 +1,26 @@
+//Q1. Write a program to read data from file.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	FILE* ptr;
+	char ch;
+
+	ptr = fopen("test.txt", "r");
+  if (NULL == ptr) {
+		printf("file can't be opened \n");
+	}
+   printf("content of this file are \n");
+
+	do {
+		ch = fgetc(ptr);
+		printf("%c", ch);
+    }
+     while (ch != EOF);
+
+	fclose(ptr);
+	return 0;
+}
